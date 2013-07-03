a98d2e82d516481bf403b237e8e2a149a1aa7642
calebmpeterson <caleb.m.peterson@gmail.com>
2013-07-03 00:27:27 -0500

    added NuGet install command to build scripts

--- build-debug.bat
+++ build-debug.bat
@@ -2,6 +2,9 @@
 
 set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
+rem use NuGet to install dependencies
+nuget install packages.config -OutputDirectory packages
+
 rmdir /q /s bin\Debug
 
 msbuild UI.csproj
