f913fb14ec9c06986a0de71ae929a6853c19a10f
Caleb Peterson <caleb.m.peterson@gmail.com>
2014-06-24 19:37:38 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-set TARGET=%CD%\bin\Release\Xel.exe
+set TARGET=%CD%\bin\Release\META-x.exe
 set STARTUP_DIR="%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-set SHORTCUT=%STARTUP_DIR%\Xel
+set SHORTCUT=%STARTUP_DIR%\META-x
 
 shortcut /target:%TARGET% /shortcut:%SHORTCUT%
